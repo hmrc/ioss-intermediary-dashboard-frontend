36a0febed1be79192b6174202021c6269f457740
Lee Powell <lee.powell@digital.hmrc.gov.uk>
2025-06-30 16:05:35 +0100

    VEI-283 | Create Intermediary Dashboard Repository

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,18 +5,20 @@ object AppDependencies {
   private val bootstrapVersion = "9.13.0"
   private val hmrcMongoVersion = "2.6.0"
 
-  val compile = Seq(
+  val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"    % "12.3.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"    % "12.6.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"    % bootstrapVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"            % hmrcMongoVersion
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"            % hmrcMongoVersion,
+    "uk.gov.hmrc"       %% "domain-play-30"                % "10.0.0",
+    "org.typelevel"     %% "cats-core"                     % "2.13.0",
   )
 
-  val test = Seq(
+  val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
-    "org.scalatestplus"       %% "scalacheck-1-17"         % "3.2.17.0"
-  ).map(_ % Test)
+    "org.scalatestplus"       %% "scalacheck-1-17"         % "3.2.18.0"
+  ).map(_ % "test, it")
 
   def apply(): Seq[ModuleID] = compile ++ test
 }
