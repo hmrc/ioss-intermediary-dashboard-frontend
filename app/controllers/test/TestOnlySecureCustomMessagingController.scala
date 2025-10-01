b4a1885bc992b907f8c0d559d4099135bc4d4ccd
Antony100 <25503510+Antony100@users.noreply.github.com>
2025-10-01 15:16:41 +0100

    add custom message creation functionality

--- app/controllers/test/TestOnlySecureCustomMessagingController.scala
+++ app/controllers/test/TestOnlySecureCustomMessagingController.scala
@@ -14,25 +14,83 @@
  * limitations under the License.
  */
 
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers.test
 
-import connectors.test.TestOnlySecureMessagingConnector
+import connectors.test.TestOnlySecureCustomMessagingConnector
+import forms.test.TestOnlySecureCustomMessagingFormProvider
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.TestOnlySecureCustomMessagingView
 
 import javax.inject.Inject
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
-case class TestOnlySecureCustomMessagingController @Inject()(
-                                                   override val messagesApi: MessagesApi,
-                                                   val controllerComponents: MessagesControllerComponents,
-                                                   view: TestOnlySecureCustomMessagingView,
-                                                 )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
-  
+class TestOnlySecureCustomMessagingController @Inject()(
+                                                         override val messagesApi: MessagesApi,
+                                                         val controllerComponents: MessagesControllerComponents,
+                                                         view: TestOnlySecureCustomMessagingView,
+                                                         connector: TestOnlySecureCustomMessagingConnector
+                                                       )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
+
+  private val form = TestOnlySecureCustomMessagingFormProvider()
+
   def onPageLoad(): Action[AnyContent] = Action {
     implicit request =>
-      Ok(view())
+      Ok(view(form))
+  }
+
+  def onSubmit(): Action[AnyContent] = Action.async {
+    implicit request =>
+      form.bindFromRequest().fold(
+        formWithErrors => Future.successful(BadRequest(view(formWithErrors))),
+        formData => {
+          val firstName: String = formData.firstName
+          val lastName: String = formData.lastName
+          val emailAddress: String = formData.emailAddress
+          val subject: String = formData.subject
+          val body: String = formData.body
+
+          connector.createSecureCustomMessage(firstName, lastName, emailAddress, subject, body).map { response =>
+            response.status match {
+              case 200 | 201 =>
+                Ok(
+                  s"""
+                     |<h1>Message successfully created!</h1>
+                     |<p><strong>Recipient:</strong> $firstName $lastName</p>
+                     |<p><strong>Sender Email:</strong>$emailAddress</p>
+                     |<p><strong>Subject:</strong> $subject</p>
+                     |<p><strong>Response Status:</strong> ${response.status}</p>
+                     |<p><a href="${controllers.test.routes.TestOnlySecureCustomMessagingController.onPageLoad()}">Create another message</a></p>
+                     |""".stripMargin
+                ).as("text/html")
+            }
+          }.recover { ex =>
+            InternalServerError(
+              s"""
+                 |<h1>Error creating message</h1>
+                 |<p><strong>Error:</strong> ${ex.getMessage}</p>
+                 |<p><a href="${controllers.test.routes.TestOnlySecureCustomMessagingController.onPageLoad()}">Go back</a></p>
+                 |""".stripMargin
+            ).as("text/html")
+          }
+        }
+      )
   }
 }
