bda761f09a4de3dea87a479aa42f02586843f323
Warren Smithson <36073378+warrenskiwork1979@users.noreply.github.com>
2025-12-03 12:05:45 +0000

    VEI-649 | Add connector to current returns endpoint. Create inital start client return controller. Clean up iports and redundant code.

--- test/connectors/RegistrationConnectorSpec.scala
+++ test/connectors/RegistrationConnectorSpec.scala
@@ -20,6 +20,7 @@ import base.SpecBase
 import com.github.tomakehurst.wiremock.client.WireMock.*
 import models.SavedPendingRegistration
 import models.domain.VatCustomerInfo
+import models.etmp.EtmpClientDetails
 import models.responses.*
 import org.scalacheck.Gen
 import play.api.Application
@@ -34,7 +35,7 @@ class RegistrationConnectorSpec extends SpecBase with WireMockHelper {
   implicit private lazy val hc: HeaderCarrier = HeaderCarrier()
 
   private val vatNumber: String = "123456789"
-  
+
   private def dashboardApplication: Application = applicationBuilder()
     .configure(
       "microservice.services.ioss-intermediary-dashboard.port" -> server.port,
